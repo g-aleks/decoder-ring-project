3b5c8ac7ad4e75735f0c5d04f7ea2a0dfc94a9b8
g-aleks <aleksandr.gonzales.98@gmail.com>
2021-06-27 12:28:59 -0600

    finished polybius pseudocode

--- src/polybius.js
+++ src/polybius.js
@@ -8,40 +8,51 @@ const polybiusModule = (function () {
 
   function polybius(input, encode = true) {
     let key = {
-      "11": "a",
-      "21": "b",
-      "31": "c",
-      "41": "d",
-      "51": "e",
-      "12": "f",
-      "22" :"g",
-      "32": "h",
-      "42": "i/j",
-      "52": "k",
-      "13": "l",
-      "23": "m",
-      "33": "n",
-      "43": "0",
-      "53": "p",
-      "14": "q",
-      "24": "r",
-      "34": "s",
-      "44": "t",
-      "54": "u",
-      "15": "v",
-      "25": "w",
-      "35": "x",
-      "45": "y",
-      "55": "z",
-    }
-  //this should create an array of each number (hi (2242) would be [22,42])  
+      11: "a",
+      21: "b",
+      31: "c",
+      41: "d",
+      51: "e",
+      12: "f",
+      22: "g",
+      32: "h",
+      42: "i/j",
+      52: "k",
+      13: "l",
+      23: "m",
+      33: "n",
+      43: "0",
+      53: "p",
+      14: "q",
+      24: "r",
+      34: "s",
+      44: "t",
+      54: "u",
+      15: "v",
+      25: "w",
+      35: "x",
+      45: "y",
+      55: "z",
+    };
+    //input.toLowerCase().split("")
+    // if !helper(input) return false
     // for let index in input
-      // if index = 0 || if index is odd {
-        // concat input[index] and input[index] + 1 and push as a string to newArray
-      //newArray.reduce((acc, letter)=>{
-        //acc.push(key[letter])
-      //},[])
+    // if input[index] = " " {push input[index]}
+    // if index = 0 || if index is odd {
+    // concat input[index] and input[index] + 1 and push as a string to numberArray
+    // let letterArray = numberArray.reduce((acc, letter)=>{
+    //if letter = " " {acc.push(letter)}
+    // else {acc.push(key[letter])}
+    // return acc
+    //},[])
+    // return letterArray.join("")
   }
+  //function helper(input)
+  //  let testArr = input.reduce((acc, entry)=>{
+  // if entry = " " {return acc}
+  // else {acc++}
+  // },0)
+  // return number % 2 === 0
 
   return {
     polybius,
