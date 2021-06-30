49e104aa787321e5976d606a607697298cb82ba5
g-aleks <aleksandr.gonzales.98@gmail.com>
2021-06-29 21:05:55 -0600

    all tests passing! goodnight!!

--- src/caesar.js
+++ src/caesar.js
@@ -7,24 +7,6 @@ const caesarModule = (function () {
   // you can add any code you want within this function scope
 
   function caesar(input, shift, encode = true) {
-    // if statement to check for !shift || <-25 || >25
-    // if encode = false, shift = shift * -1
-    // 
-    // array of letters "abcdefghijklmnopqrstuvwxyz".split("")
-    // input.toLowerCase.split("")
-    // create empty string to add results to
-    // for each letter of input
-      // if (!letter.match/[a-z]/) add letter (do not shift)
-      // get index of letter in alphabet
-      // add shift to index
-      // if > 25, newIndex -26 : <-25, newIndex + 26
-      // add letter from alphabet at newIndex
-    // return now populated string
-    let word = "hi"
-    return word;
-  }
-
-  function caesar(input, shift, encode = true) {
     encode? console.log(`Encoding...`) : console.log(`Decoding...`)
     if (!shift || shift < -25 || shift > 25) return false;
     if (encode === false) shift*= -1;
--- src/substitution.js
+++ src/substitution.js
@@ -6,19 +6,58 @@
 const substitutionModule = (function () {
   // you can add any code you want within this function scope
 
+  
   function substitution(input, alphabet, encode = true) {
-    // let realAlphabet = "abcdefghijklmnopqrstuvwxyz".split("")
-    // let decoded = [],
-    // for (let i = 0; i<alphabet.length; i++){
-    //   let fakeLetter = alphabet[i]
-    //   let newObj = {fakeLetter: realAlphabet[i]}
-    //   decoded.push(newObj)
-    // }
-    // let results = input.reduce((acc, letter)=>{
-    //   acc.push(decoded[letter])
-    //   return acc;
-    // },[])
-    // return results.join("")
+    if (!alphabet || alphabet.length != 26 ) return false
+    let realAlphabet = "abcdefghijklmnopqrstuvwxyz".split("")
+    let decoded = [];
+    let duplicateChecker = [];
+    for (let i=0; i<alphabet.length; i++){
+      let fakeLetter = alphabet[i]
+      if (duplicateChecker.includes(fakeLetter)){
+        return false;
+      }
+      duplicateChecker.push(fakeLetter)
+      let newObj = {[fakeLetter]: realAlphabet[i]}
+      decoded.push(newObj)
+    }
+    let inputArray = input.split("")
+    if (!encode) return _decoder(inputArray, decoded)
+    return _encoder(inputArray, decoded)
+  }
+
+  function _encoder(input, decoded){
+    let results = input.reduce((acc, letter)=>{
+      if (letter === " "){
+        acc.push(letter)
+        return acc
+      }
+      for (let object of decoded){
+        let values = Object.values(object)
+        if (values.includes(letter)){ 
+          let test = Object.keys(object)
+          acc.push(test[0])
+        }
+      }
+      return acc
+    }, [])
+    return results.join("")
+  }
+
+  function _decoder(inputArray, decoded){
+    let results = inputArray.reduce((acc, letter)=>{
+      if (letter === " "){
+        acc.push(letter)
+        return acc;
+      }
+      for (let object of decoded){
+        if (object[letter]){
+          acc.push(object[letter])
+        }
+      }
+      return acc;
+    },[])
+    return results.join("")
   }
 
   return {
