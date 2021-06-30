3f0434f2cd126f6a8816a4719d4020ac7c6f9d82
g-aleks <aleksandr.gonzales.98@gmail.com>
2021-06-30 17:19:20 -0600

    removed instructions from caesar test file

--- test/caesar.test.js
+++ test/caesar.test.js
@@ -2,14 +2,6 @@
 const expect = require("chai").expect;
 const { caesar } = require("../src/caesar");
 
-// For the Caesar shift (example: caesar("Zebra Magazine", 3) => "cheud pdjdclqh"), the tests that you write should test that the following is true:
-
-//     It returns false if the shift value is equal to 0, less than -25, greater than 25, or not present.
-//     It ignores capital letters. (For example, the results of A Message and a message should be the same.)
-//     When encoding, it handles shifts that go past the end of the alphabet. (For example, shifting z to the right by 3 should cause the z to wrap around to the front of the alphabet, so that z becomes c.)
-//     It maintains spaces and other nonalphabetic symbols in the message, before and after encoding or decoding.
-
-
 describe("Caesar", () => {
 
     describe("Error Handling", ()=>{
